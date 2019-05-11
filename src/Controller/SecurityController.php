6258de21aeaea76b869c1b28e59a25ef60d9247f
Fantin Bibas <fantin.bibas@epitech.eu>
2019-05-11 16:22:06 +0200

    Working user creation / connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends AbstractController
 {
-    public function createJsonWebToken(Request $request)
+    public function createJsonWebToken(Request $request, JWTTokenManagerInterface $jwtManager)
     {
+        return new JsonResponse([
+            'token' => $jwtManager->create($this->getUser()),
+        ]);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,12 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
-    public function createUser(Request $request)
+    public function createUser(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
+        // TODO
+        if (!$request->getContentType() === 'application/json')
+            return new JsonResponse([], 400);
+        $userData = json_decode($request->getContent(), true);
+        $user = new User();
+        $user->setEmail($userData['email']);
+        $user->setPassword($passwordEncoder->encodePassword($user, $userData['password']));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+        return new JsonResponse([
+            'user' => $user,
+        ]);
     }
 }
