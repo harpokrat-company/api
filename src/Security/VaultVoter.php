7e63c2aa02a5e1bb233481278bd8b388c55b411b
Louis Mallez <louis.mallez@epitech.eu>
2020-09-09 22:41:54 +0200

    Vault voter

---
+++ src/Security/VaultVoter.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace App\Security;
+
+
+use App\Entity\OrganizationGroup;
+use App\Entity\User;
+use App\Entity\Vault;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class VaultVoter extends ResourceVoter
+{
+    public function __construct()
+    {
+        parent::__construct(Vault::class);
+    }
+
+    protected function getAttributesFunctions(): array
+    {
+        $owner = function ($subject, TokenInterface $token) {
+            /** @var User $user */
+            if (!$user = $token->getUser()) {
+                return false;
+            }
+            return $this->isOwner($subject, $user);
+        };
+        $member = function ($subject, TokenInterface $token) {
+            /** @var User $user */
+            if (!$user = $token->getUser()) {
+                return false;
+            }
+            return $this->isMember($subject, $user);
+        };
+        return [
+            /* 'create' => $member, TODO : fix this*/
+            'view' => $member,
+            'edit' => $owner,
+            'edit-secrets' => $member,
+            'delete' => $owner,
+        ];
+    }
+
+    public function isOwner(Vault $vault, User $user) {
+        $owner = $vault->getOwner();
+        if ($owner instanceof User)
+            return $owner === $user;
+        if ($owner instanceof OrganizationGroup)
+            return $owner->getOrganization()->getOwner() === $user;
+        return false;
+    }
+
+    public function isMember(Vault $vault, User $user) {
+        $owner = $vault->getOwner();
+        if ($owner instanceof User)
+            return $owner === $user;
+        if ($owner instanceof OrganizationGroup)
+            return $owner->getMembers()->contains($user);
+        return false;
+    }
+}
